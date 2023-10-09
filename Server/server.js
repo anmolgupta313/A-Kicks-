69ed05e72454703c1bb1a1cf615c4f43cf25f9d3
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-10-09 18:20:54 -0400

    I have moved the vercel.json file to the public

--- Server/server.js
+++ Server/server.js
@@ -16,8 +16,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
