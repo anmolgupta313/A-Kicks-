c4d9eb59c8e7ddbaf74c910a06c26c13093dfaa0
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-10-09 17:05:59 -0400

    I have updated config file according to heroku

--- Server/server.js
+++ Server/server.js
@@ -1,29 +1,27 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 const cors = require("cors");
 // import sequelize connection
-const sequelize = require('./config/connection')
-const cookieParser= require('cookie-parser')
+const sequelize = require("./config/connection");
+const cookieParser = require("cookie-parser");
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3001;
- 
 
 // const  corsOptions = {
 //   origin: "http://localhost:3001"
 // };
 
-app.use(cors())
-app.use(cookieParser())
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({force:true}).then(()=>{
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port !`);
   });
-  
-})
+});
